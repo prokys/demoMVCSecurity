83648e1d24aa753e4c1e5f8bad02e71f43e5dd1b
Prokop Ferdan <prokes1@gmail.com>
2024-08-21 11:21:46 +0200

    added Logout button to home page, updated fancy-login page to show if user has been logged out, updated DemoSecurityConfig with logout

--- src/main/java/com/prokys/demoMVCSecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/prokys/demoMVCSecurity/security/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.prokys.demoMVCSecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -25,7 +26,7 @@ public class DemoSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
-                .formLogin(form -> form.loginPage(("/showMyLoginPage")).loginProcessingUrl("/authenticateTheUser").permitAll());
+                .formLogin(form -> form.loginPage(("/showMyLoginPage")).loginProcessingUrl("/authenticateTheUser").permitAll()).logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
