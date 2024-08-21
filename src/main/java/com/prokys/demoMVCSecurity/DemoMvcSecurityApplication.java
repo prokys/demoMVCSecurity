f0f3f985a1bbdfff82786d987ac9b3813ae8c05d
Prokop Ferdan <prokes1@gmail.com>
2024-08-21 10:16:51 +0200

    initializing project

---
+++ src/main/java/com/prokys/demoMVCSecurity/DemoMvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.prokys.demoMVCSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prokys/demoMVCSecurity/DemoMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokys.demoMVCSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
